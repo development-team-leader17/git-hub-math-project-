efaeff05f9c6d5f60c77fc07289c79dd1708b142
Thna17 <hangbrathna10@gmai.com>
2024-03-20 14:06:14 +0700

    issue5

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 radius = 7
-print('The circumference of the circle :', 2 * 3.14 * radius)
-print('The area of the circle :', 3.14 * radius ** 2)
+print('The circumference of the circle :', 2 * 3.15* radius)
+print('The area of the circle :', 3.15 * radius ** 2)
 print('The radius of the cicle :', radius)
