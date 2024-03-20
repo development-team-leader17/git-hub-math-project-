319fac67151e575ead3a640f61dfb929d2a65b26
Thna17 <hangbrathna10@gmai.com>
2024-03-20 13:30:20 +0700

    issue2 : delete variable PI

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 radius = 7
-PI = 3.14
-circumference = 2 * PI * radius
-area = PI * radius ** 2
+circumference = 2 * 3.14 * radius
+area = 3.14 * radius ** 2
 print('The circumference of the circle :', circumference)
 print('The area of the circle :', area)
 print('The radius of the cicle :', radius)
